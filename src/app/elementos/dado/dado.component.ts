e6ee9daefc9ba7f0b1f5ee789ef36c66b034e08a
jefry145 <116960895+jefry145@users.noreply.github.com>
2022-12-29 11:12:54 -0500

    Add files via upload

---
+++ src/app/elementos/dado/dado.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dado',
+  templateUrl: './dado.component.html',
+  styleUrls: ['./dado.component.css']
+})
+export class DadoComponent {
+   number = 0
+   num_ramdon(){
+      this.number = Math.round((Math.random()));
+   }
+}
